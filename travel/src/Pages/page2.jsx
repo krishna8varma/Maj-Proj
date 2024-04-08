61f1eaf0cf61a70ee509e283c493da00ab705bda
krishna8varma <94862972+krishna8varma@users.noreply.github.com>
2024-04-08 14:01:22 +0530

    updated

--- travel/src/Pages/page2.jsx
+++ travel/src/Pages/page2.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "../Components/Navbar/navbar";
- 
+import img from "../Assets/Pag1bg.jpg"
 const Page1 = () => {
     return (
+        <section className="page1"> 
         <div>
            <Navbar/>
         </div>
+       
+        <div className="content">
+
+        <img src={img} alt="ocean" />
+        </div>
+        </section>
     );
 };
  
