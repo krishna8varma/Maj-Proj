e9ea9c94b6e8522a9d55b53f223798a8e4f10522
krishna8varma <94862972+krishna8varma@users.noreply.github.com>
2024-04-08 20:54:42 +0530

    Revert "updated"
    This reverts commit 61f1eaf0cf61a70ee509e283c493da00ab705bda.

--- travel/src/Pages/page1.jsx
+++ travel/src/Pages/page1.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar/navbar";
-import img from "../Assets/Page1bg.jpg";
-import Footer from "../Components/Footer/footer"
-import './page1.css'
+ 
 const Page1 = () => {
     return (
-        <section className="page1"> 
         <div>
            <Navbar/>
         </div>
-       
-        <div className="content">
-
-        <img src={img} alt="ocean" />
-        <h1>Who are you travelling with?</h1>
-        </div>
-
-
-        <Footer/>
-        </section>
     );
 };
  
